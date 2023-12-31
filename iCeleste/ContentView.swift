012bd90566ebf0f3cfba8c213c5ffa1ba1fda862
BomberFish <bomberfish@bomberfish.ca>
2023-12-30 21:36:58 -0500

    Initial Commit

---
+++ iCeleste/ContentView.swift
@@ -0,0 +1,21 @@
+// bomberfish
+// ContentView.swift – iCeleste
+// created on 2023-12-30
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCeleste/iCelesteApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// iCelesteApp.swift – iCeleste
+// created on 2023-12-30
+
+import SwiftUI
+
+@main
+struct iCelesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
